cf5fc3725cf09de088b09c5a23dae27d5f83e83b
Sumayah Sandhu <sumayah@ou.edu>
2020-09-10 15:55:16 -0500

    Fizz Buzz participation

---
+++ Fizz Buzz/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+
+            if (number % 5 == 0 && number % 3 == 0)
+
+
+
+
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+
+
+
+            else if (number % 3 == 0)
+
+            {
+                Console.WriteLine("Fizz");
+
+            }
+
+
+            else if (number % 5 == 0)
+
+            {
+                Console.WriteLine("Buzz");
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            else Console.WriteLine(number);
+        }
+    }
+}
