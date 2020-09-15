91b6981bfe70546ecbf90f7079e7e510b0e6a98e
Sumayah Sandhu <sumayah@ou.edu>
2020-09-15 12:34:45 -0500

    Added our solutions and existing projects

---
+++ Participations/Loops_GuessANumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loops_GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Participations/Loopsinclass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loopsinclass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0; 
+
+            for (int i = 1, i<101; i++)
+
+            {
+               sum=sum+i; 
+            }
+
+            Console.WriteLine($"The sum of the value from 1-100 is {sum}"); 
+            
+            int iterations = 1; 
+
+        }
+    }
+}
