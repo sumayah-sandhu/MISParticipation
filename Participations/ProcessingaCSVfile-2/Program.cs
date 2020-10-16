16d1bb37864d286c582fc56a96d9fb0dd38e6c4f
Sumayah Sandhu <sumayah@ou.edu>
2020-10-15 22:50:39 -0500

    Finished second CSV participation

---
+++ Participations/ProcessingaCSVfile-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessingaCSVfile_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String filepath = @"SalesJan2009.csv";
+            String[] lines = File.ReadAllLines(filepath);
+
+           
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] splitlines = line.Split(',');
+                string name = splitlines[4];
+                string transdate = splitlines[0];
+
+                if (splitlines[3].ToLower() == "visa" & splitlines[7].ToLower() == "united states")
+                {
+
+
+                    Console.WriteLine(name + " " + transdate);
+                    
+
+
+                }
+
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
